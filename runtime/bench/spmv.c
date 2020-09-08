4d89dd1f7ffa49eb734e6329e43623c1843aa561
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-08 01:22:03 -0400

    comment

--- runtime/bench/spmv.c
+++ runtime/bench/spmv.c
@@ -1,3 +1,12 @@
+/*
+compiled using
+https://godbolt.org/
+gcc 9.3
+-O3 -m64 -march=x86-64 -mtune=x86-64 -fopt-info-vec -mavx -fomit-frame-pointer -DNDEBUG
+./gen_rollforward.sh spmv spmv
+ */
+
+
 #include <stdint.h>
 
 void spmv_serial(
