ac4df443abceb84f9b096fa35d337f24939aa99c
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-14 00:24:51 -0400

    edit

--- runtime/bench/incr_array_orig.hpp
+++ runtime/bench/incr_array_orig.hpp
@@ -1,6 +1,40 @@
-int64_t* incr_array_interrupt(int64_t* a, uint64_t lo, uint64_t hi, void* p) {
-  for (uint64_t i = lo; i != hi; i++) {
-    a[i]++;
+#include <cstdint>
+
+void incr_array_serial(int64_t* a, uint64_t lo, uint64_t hi) {
+  for (; lo != hi; lo++) {
+    a[lo]++;
   }
-  return a;
+}
+
+#define MIN(x, y) ((x < y) ? x : y)
+
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+
+#define D 128
+
+extern volatile
+int heartbeat;
+ 
+extern
+int incr_array_handler(int64_t* a, uint64_t lo, uint64_t& hi, void* p);
+
+void incr_array_interrupt(int64_t* a, uint64_t lo, uint64_t hi, void* p) {
+  if (! (lo < hi)) {
+    return;
+  }
+  for (;;) {
+    uint64_t lo2 = lo;
+    uint64_t hi2 = MIN(lo + D, hi);
+    for (; lo2 != hi2; lo2++) {
+      a[lo2]++;
+    }
+    lo = lo2;
+    if (unlikely(! (lo < hi))) {
+      break;
+    }
+    if (unlikely(heartbeat)) { 
+      incr_array_handler(a, lo, hi, p);
+    }
+  }
 }
