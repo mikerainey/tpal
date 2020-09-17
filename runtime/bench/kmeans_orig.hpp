224564dcc85fd149671159667750aa78a55c2b5d
mikerainey <bionic.oldsmobile@gmail.com>
2020-09-17 01:58:51 -0400

    working on kmeans

--- runtime/bench/kmeans_orig.hpp
+++ runtime/bench/kmeans_orig.hpp
@@ -1,3 +1,10 @@
+/*
+compiled using
+https://godbolt.org/
+gcc 9.3
+-O3 -m64 -march=x86-64 -mtune=x86-64 -fopt-info-vec -mavx -fomit-frame-pointer -DNDEBUG
+ */
+
 #include <stdlib.h>
 
 #define RANDOM_MAX 2147483647
@@ -43,84 +50,155 @@ int find_nearest_point(float  *pt,          /* [nfeatures] */
   return(index);
 }
 
-float** kmeans_interrupt(float **feature,    /* in: [npoints][nfeatures] */
-                          int     nfeatures,
-                          int     npoints,
-                          int     nclusters,
-                          float   threshold,
-                          int    *membership) /* out: [npoints] */
+#define MIN(x, y) ((x < y) ? x : y)
+
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+
+#define D 64
+
+extern volatile
+int heartbeat;
+
+extern
+int kmeans_outer_handler(float **feature,
+                  int     nfeatures,
+                  int     npoints,
+                  int     nclusters,
+                  float   threshold,
+                  int    *membership,
+                  float   delta,
+                  float  **clusters,
+                  int     *new_centers_len,
+                  float  **new_centers,
+                  int      lo,
+                  int      hi,
+                  void* p);
+
+void kmeans_outer(float **feature,
+                  int     nfeatures,
+                  int     npoints,
+                  int     nclusters,
+                  float   threshold,
+                  int    *membership,
+                  float   delta,
+                  float  **clusters,
+                  int     *new_centers_len,
+                  float  **new_centers,
+                  void    *p) 
 {
 
-  int      i, j, n=0, index, loop=0;
-  int     *new_centers_len; /* [nclusters]: no. of points in each cluster */
-  float    delta;
-  float  **clusters;   /* out: [nclusters][nfeatures] */
-  float  **new_centers;     /* [nclusters][nfeatures] */
-  
-
-  /* allocate space for returning variable clusters[] */
-  clusters    = (float**) malloc(nclusters *             sizeof(float*));
-  clusters[0] = (float*)  malloc(nclusters * nfeatures * sizeof(float));
-  for (i=1; i<nclusters; i++)
-    clusters[i] = clusters[i-1] + nfeatures;
-
-  /* randomly pick cluster centers */
-  for (i=0; i<nclusters; i++) {
-    //n = (int)rand() % npoints;
-    for (j=0; j<nfeatures; j++)
-      clusters[i][j] = feature[n][j];
-    n++;
-  }
-
-  for (i=0; i<npoints; i++)
-    membership[i] = -1;
-
-  /* need to initialize new_centers_len and new_centers[0] to all 0 */
-  new_centers_len = (int*) calloc(nclusters, sizeof(int));
-
-  new_centers    = (float**) malloc(nclusters *            sizeof(float*));
-  new_centers[0] = (float*)  calloc(nclusters * nfeatures, sizeof(float));
-  for (i=1; i<nclusters; i++)
-    new_centers[i] = new_centers[i-1] + nfeatures;
- 
-  
   do {
-		
-    delta = 0.0;
-
-    for (i=0; i<npoints; i++) {
-      /* find the index of nestest cluster centers */
-      index = find_nearest_point(feature[i], nfeatures, clusters, nclusters);
-      /* if membership changes, increase delta by 1 */
-      if (membership[i] != index) delta += 1.0;
-
-      /* assign the membership to object i */
-      membership[i] = index;
-
-      /* update new cluster centers : sum of objects located within */
-      new_centers_len[index]++;
-      for (j=0; j<nfeatures; j++)          
-	      new_centers[index][j] += feature[i][j];
-    }
       
+      delta = 0.0;
 
-    /* replace old cluster centers with new_centers */
-    for (i=0; i<nclusters; i++) {
-      for (j=0; j<nfeatures; j++) {
-	      if (new_centers_len[i] > 0)
-	        clusters[i][j] = new_centers[i][j] / new_centers_len[i];
-	        new_centers[i][j] = 0.0;   /* set back to 0 */
+      int lo = 0;
+      int hi = npoints;
+      if (! (lo < hi)) {
+        return;
       }
-      new_centers_len[i] = 0;   /* set back to 0 */
-    }
-            
-    //delta /= npoints;
-  } while (delta > threshold);
+      for (;;) {
+        int lo2 = lo;
+        int hi2 = MIN(lo + D, hi);
+        for (; lo2<hi2; lo2++) {
+          /* find the index of nestest cluster centers */
+          int index = find_nearest_point(feature[lo2], nfeatures, clusters, nclusters);
+          /* if membership changes, increase delta by 1 */
+          if (membership[lo2] != index) delta += 1.0;
 
-  
-  free(new_centers[0]);
-  free(new_centers);
-  free(new_centers_len);
+          /* assign the membership to object i */
+          membership[lo2] = index;
 
-  return clusters;
+          /* update new cluster centers : sum of objects located within */
+          new_centers_len[index]++;
+          for (int j=0; j<nfeatures; j++)          
+            new_centers[index][j] += feature[lo2][j];
+        }
+        lo = lo2;
+        if (unlikely(! (lo < hi))) {
+          break;
+        }
+        if (unlikely(heartbeat)) {
+            if (kmeans_outer_handler(feature, nfeatures, npoints, nclusters, threshold, membership, delta, clusters, new_centers_len, new_centers, lo, hi, p)) {
+              return;
+            }
+          }
+      }
+        
+
+      /* replace old cluster centers with new_centers */
+      for (int i=0; i<nclusters; i++) {
+        for (int j=0; j<nfeatures; j++) {
+          if (new_centers_len[i] > 0)
+            clusters[i][j] = new_centers[i][j] / new_centers_len[i];
+            new_centers[i][j] = 0.0;   /* set back to 0 */
+        }
+        new_centers_len[i] = 0;   /* set back to 0 */
+      }
+              
+      //delta /= npoints;
+    } while (delta > threshold);
+                          
+}
+
+extern
+int kmeans_inner_handler(float **feature,
+                  int     nfeatures,
+                  int     npoints,
+                  int     nclusters,
+                  float   threshold,
+                  int    *membership,
+                  float   delta,
+                  float  **clusters,
+                  int     *new_centers_len,
+                  float  **new_centers,
+                  int      lo,
+                  int      hi,
+                  void* p);
+
+void kmeans_inner(float **feature,
+                  int     nfeatures,
+                  int     npoints,
+                  int     nclusters,
+                  float   threshold,
+                  int    *membership,
+                  float   delta,
+                  float  **clusters,
+                  int     *new_centers_len,
+                  float  **new_centers,
+                  int      lo,
+                  int      hi,
+                  void    *p) 
+{
+      if (! (lo < hi)) {
+        return;
+      }
+      for (;;) {
+        int lo2 = lo;
+        int hi2 = MIN(lo + D, hi);
+        for (; lo2<hi2; lo2++) {
+          /* find the index of nestest cluster centers */
+          int index = find_nearest_point(feature[lo2], nfeatures, clusters, nclusters);
+          /* if membership changes, increase delta by 1 */
+          if (membership[lo2] != index) delta += 1.0;
+
+          /* assign the membership to object i */
+          membership[lo2] = index;
+
+          /* update new cluster centers : sum of objects located within */
+          new_centers_len[index]++;
+          for (int j=0; j<nfeatures; j++)          
+            new_centers[index][j] += feature[lo2][j];
+        }
+        lo = lo2;
+        if (unlikely(! (lo < hi))) {
+          break;
+        }
+        if (unlikely(heartbeat)) {
+            if (kmeans_inner_handler(feature, nfeatures, npoints, nclusters, threshold, membership, delta, clusters, new_centers_len, new_centers, lo, hi, p)) {
+              return;
+            }
+          }
+      }
+                          
 }
