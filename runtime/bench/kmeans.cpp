053dfe2b4accacd5bd583a8af7cd783a1b654325
mikerainey <bionic.oldsmobile@gmail.com>
2020-10-09 19:32:47 -0400

    change input flag of kmeans

--- runtime/bench/kmeans.cpp
+++ runtime/bench/kmeans.cpp
@@ -6,7 +6,7 @@ namespace tpalrts {
 using namespace kmeans;
   
 void launch() {
-  numObjects = deepsea::cmdline::parse_or_default_int("n", numObjects);
+  numObjects = deepsea::cmdline::parse_or_default_int("nb_objects", numObjects);
   using microbench_scheduler_type = mcsl::minimal_scheduler<stats, logging, mcsl::minimal_elastic, tpal_worker>;
   auto bench_body_manual = new tpalrts::terminal_fiber<microbench_scheduler_type>();
   launch(bench_pre, bench_body_interrupt, bench_body_software_polling, bench_body_serial,
