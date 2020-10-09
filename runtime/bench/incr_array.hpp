ed20325e4ed5b49c3c4327f498f50750b227a7a2
mikerainey <bionic.oldsmobile@gmail.com>
2020-10-09 19:32:17 -0400

    increase size of input for incr_array

--- runtime/bench/incr_array.hpp
+++ runtime/bench/incr_array.hpp
@@ -103,7 +103,7 @@ namespace incr_array {
 
 char* name = "incr_array";
   
-uint64_t nb_items = 100 * 1000 * 1000;
+uint64_t nb_items = 1000 * 1000 * 1000;
 int64_t* a;
   
 auto bench_pre(promotable*) -> void {
