1a077a6274a988e59dda6010b860496166732d95
mikerainey <bionic.oldsmobile@gmail.com>
2020-11-05 22:26:09 -0500

    fix

--- runtime/bench/srad.hpp
+++ runtime/bench/srad.hpp
@@ -234,6 +234,61 @@ void srad_cilk() {
 #endif
 }
 
+void srad_serial2(int rows, int cols, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
+  for (int i = 0 ; i < rows ; i++) {
+    for (int j = 0; j < cols; j++) { 
+		
+      int k = i * cols + j;
+      float Jc = J[k];
+ 
+      // directional derivates
+      dN[k] = J[iN[i] * cols + j] - Jc;
+      dS[k] = J[iS[i] * cols + j] - Jc;
+      dW[k] = J[i * cols + jW[j]] - Jc;
+      dE[k] = J[i * cols + jE[j]] - Jc;
+			
+      float G2 = (dN[k]*dN[k] + dS[k]*dS[k] 
+		  + dW[k]*dW[k] + dE[k]*dE[k]) / (Jc*Jc);
+
+      float L = (dN[k] + dS[k] + dW[k] + dE[k]) / Jc;
+
+      float num  = (0.5*G2) - ((1.0/16.0)*(L*L)) ;
+      float den  = 1 + (.25*L);
+      float qsqr = num/(den*den);
+ 
+      // diffusion coefficent (equ 33)
+      den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
+      c[k] = 1.0 / (1.0+den) ;
+                
+      // saturate diffusion coefficent
+      if (c[k] < 0) {c[k] = 0;}
+      else if (c[k] > 1) {c[k] = 1;}
+   
+    }
+  
+  }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {        
+
+      // current index
+      int k = i * cols + j;
+                
+      // diffusion coefficent
+      float cN = c[k];
+      float cS = c[iS[i] * cols + j];
+      float cW = c[k];
+      float cE = c[i * cols + jE[j]];
+
+      // divergence (equ 58)
+      float D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
+                
+      // image update (equ 61)
+      J[k] = J[k] + 0.25*lambda*D;
+    }
+  }
+}
+
+
 namespace srad {
 
 using namespace tpalrts;
@@ -333,7 +388,7 @@ auto bench_body_software_polling(promotable* p) {
 };
 
 auto bench_body_serial(promotable* p) {
-  srad_serial(rows, cols, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda);
+  srad_serial2(rows, cols, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda);
 };
 
 auto bench_post(promotable* p) {
